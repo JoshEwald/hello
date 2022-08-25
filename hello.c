53cec631bbfba43bfd263bd112684f754936dc12
JoshEwald <joshuaewald@unomaha.edu>
2022-08-25 19:02:30 +0000

    format change

--- hello.c
+++ hello.c
@@ -12,11 +12,10 @@
 int main(int argc, char **argv) {
 
   printf("*My name is Joshua Ewald*\n");
-
   printf("*My major is Computer Engineering*\n");
 
   printf("Kristian Patdu!\n");
   printf("Computer Engineering!\n");
-  
+
   return 0;
 }
